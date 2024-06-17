5d5f6fac2107b2275e78419a91c0cc69e8405d0b
Riccardo Malchiodi <115894161+Malchio03@users.noreply.github.com>
2024-06-17 21:09:43 +0200

    Add files via upload

---
+++ Uni/2023-2024/ASD/Laboratori Da Implementare/asd-lab4-traccia/ASD-stack.cpp
@@ -0,0 +1,33 @@
+// FILE DA MODIFICARE
+#include "ASD-stack.h"
+
+using namespace stack;
+using namespace std;
+
+/**************************************************/
+/*      funzioni da implementare                  */
+/**************************************************/
+/* restituisce lo stack vuoto */
+Stack stack::createEmpty(){
+
+}
+
+/* restituisce true se lo stack e' vuoto */
+bool stack::isEmpty(const Stack& st){
+    
+}
+
+/* aggiunge elem in cima (operazione safe, si può sempre fare) */
+/* NB: se stack implementato con array dinamico, 
+   quando necessario implementare ridimensionamento in espansione*/
+void stack::push(const Elem el, Stack& st){
+    
+}
+
+/* toglie dallo stack l'ultimo elemento e lo restituisce */
+/* se lo stack è vuoto solleva una eccezione di tipo string */
+/* NB: se stack implementato con array dinamico, 
+   quando necessario implementare ridimensionamento in contrazione*/
+Elem stack::pop(Stack& st){
+
+} 
---
+++ Uni/2023-2024/ASD/Laboratori Da Implementare/asd-lab4-traccia/ASD-token.cpp
@@ -0,0 +1,20 @@
+//FILE DA MODIFICARE
+#include <iostream>
+#include "ASD-token.h"
+
+using namespace std;
+
+/**************************************************/
+/*      funzioni da implementare                  */
+/**************************************************/
+
+//funzione che estrae il prossimo token della string st
+//lo mette in tok e modifica st,
+//ritorna true se c'era un token da estrarre,
+//ritorna false se non c'era niente da estrarre e si e arrivato alla fine di st,
+//solleva una eccezione di tipo string se legge un token di tipo sconosciuto
+bool nextToken(string &st, token &tok) {
+  //TODO
+  return true;
+}
+
