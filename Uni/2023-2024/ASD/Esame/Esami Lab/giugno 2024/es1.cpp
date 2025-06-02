280734a44f86a1db6dbd0145b02b65ce319a5d8c
Riccardo Malchiodi <115894161+Malchio03@users.noreply.github.com>
2025-06-02 17:17:27 +0200

    Soluzione es1 ASD esame 2024

--- Uni/2023-2024/ASD/Esame/Esami Lab/giugno 2024/es1.cpp
+++ Uni/2023-2024/ASD/Esame/Esami Lab/giugno 2024/es1.cpp
@@ -1,6 +1,18 @@
 #include "trinary-tree.h"
 
 unsigned int nbOccurrence(Elem el,const triTree& tr){
-  //DA MODIFICARE
-  return 1000;
+
+  if (tr == emptyTriTree) return 0;
+
+  unsigned int count = 0;
+
+  if (tr -> el == el) {
+    ++count;
+  }
+
+  count += nbOccurrence(el, tr -> child1);
+  count += nbOccurrence(el, tr -> child2);
+  count += nbOccurrence(el, tr -> child3);
+
+  return count;
 }
