8852f9df5bb4da56b8ba39602fcfc75a0ad2809c
Riccardo Malchiodi <115894161+Malchio03@users.noreply.github.com>
2026-09-17 14:38:02 +0200

    TAP Febbraio

---
+++ Uni/2025-2026/TAP/Esami/2025/Febbraio/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace Esercizio1;
+
+public static class Class1
+{
+    public static IEnumerable<T>MyTake<T>(this IEnumerable<T> source, int count)
+    {
+        if(source == null)
+            throw new ArgumentNullException(nameof(source));
+
+        if(count < -1)
+            throw new ArgumentOutOfRangeException(nameof(count));
+
+        return Iterator();
+
+        IEnumerable<T> Iterator()
+        {
+            if(count == 0)
+                yield break;
+
+            int contatore = 0;
+            
+            foreach(var item in source)
+            {
+                if(contatore < count)
+                {
+                    yield return item;
+                    contatore++;
+                }
+
+                if(count == -1)
+                    yield return item;
+            }
+        }
+    }
+}
---
+++ Uni/2025-2026/TAP/Esami/2025/Febbraio/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿namespace Esercizio3;
+using Esercizio1;
+using Moq;
+
+public interface ITextOutput {
+    void Write(string s);
+    }
+    public class MyPrinter {
+    private readonly ITextOutput _output;
+    public MyPrinter(ITextOutput output) => _output = output;
+    public void PrintFirstN<T>(IEnumerable<T> source, int count) 
+    {
+        foreach(var el in source.Take(count))
+        _output.Write(el?.ToString() ?? "boh");
+    }
+}
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var moq = new Mock<ITextOutput>();
+        var service = new MyPrinter(moq.Object);
+        var source = new List<int>{1,2,3};
+        service.PrintFirstN(source, 2);
+
+        moq.Verify(c => c.Write("1"), Times.Once);
+        moq.Verify(c => c.Write("2"), Times.Once);
+        moq.Verify(c => c.Write(It.IsAny<string>()), Times.Exactly(2));
+    }
+}
